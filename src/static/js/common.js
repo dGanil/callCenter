6930cee75c952e1727559a198cc6f9a4a5ea11dd
dGanil <desuhonor@yandex.ru>
2021-05-28 18:26:11 +0300

    adaptive 1

--- src/static/js/common.js
+++ src/static/js/common.js
@@ -1,22 +1,41 @@
 $(document).ready(function() {
-    // var swiper = new Swiper('.swiper-container', {
-    //     slidesPerView: 3,
-    //     spaceBetween: 30,
-    //     navigation: {
-    //      nextEl: '.arrow-next',
-    //      prevEl: '.arrow-prev'
-    //  }
-    //   })
+    if ($(window).width() <= 1400 && $(window).width() > 991) {
+        var swiper = new Swiper('.swiper-container', {
+            navigation: {
+                nextEl: '.arrow-next',
+                prevEl: '.arrow-prev',
+            },
+            slidesPerView: 2,
+            spaceBetween: 41,
+            slidesPerGroup: 1,
+            loop: true,
+            loopFillGroupWithBlank: true,
+        });
+    } else if ($(window).width() > 1400) {
+        var swiper = new Swiper('.swiper-container', {
+            navigation: {
+                nextEl: '.arrow-next',
+                prevEl: '.arrow-prev',
+            },
+            slidesPerView: 3,
+            spaceBetween: 41,
+            slidesPerGroup: 1,
+            loop: true,
+            loopFillGroupWithBlank: true,
+        });
+    } else if ($(window).width() <= 991) {
+        var swiper = new Swiper('.swiper-container', {
+            navigation: {
+                nextEl: '.arrow-next',
+                prevEl: '.arrow-prev',
+            },
+            slidesPerView: 1,
+            spaceBetween: 41,
+            slidesPerGroup: 1,
+            loop: true,
+            loopFillGroupWithBlank: true,
+        });
+    }
 
-    var swiper = new Swiper('.swiper-container', {
-        navigation: {
-            nextEl: '.arrow-next',
-            prevEl: '.arrow-prev',
-        },
-        slidesPerView: 3,
-        spaceBetween: 41,
-        slidesPerGroup: 1,
-        loop: true,
-        loopFillGroupWithBlank: true,
-    });
+
 })
